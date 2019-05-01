fb6b8972ef182db5974ed4741061bb9eee646965
Aykaz Madzharyan <Aykaz@aykazs-mac-mini.local>
2019-05-01 17:31:59 +0300

    Initial Commit

---
+++ To-Do-List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To-Do-List
+//
+//  Created by Aykaz on 01/05/2019.
+//  Copyright © 2019 Aykaz Madzharyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
